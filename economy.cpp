4c1a44d614f67e0087dc01f4e0198298c221ab40
Lucas Belmudes <belmu002@umn.edu>
2024-01-22 11:36:36 -0600

    Testing full code, 10 threads

--- economy.cpp
+++ economy.cpp
@@ -9,7 +9,7 @@
 //! Note: For computations we first iterate over the low recovery bond and then iterate over the high recovery bond.
 //! Vectors are then V=[(b_lowr1;b_highr1;y_1), (b_lowr2;b_highr1;y_1),...,(b_lowrn;b_highr1;y_1),(b_lowr1;b_highr2;y_1),...
 
-const int MAX_THREADS = 32; // Define a constant variable at the start of the file
+const int MAX_THREADS = 10; // Define a constant variable at the start of the file
 
 Economy::Economy(int b_grid_size_lowr, int b_grid_size_highr, double b_grid_min_lowr, double b_grid_min_highr, double b_grid_max_lowr, double b_grid_max_highr, int y_grid_size, double y_default, double beta, double gamma, double r, double rho, double sigma, double theta, double alpha_lowr, double alpha_highr, double tol, int max_iter, double m, double* ptr_y_grid, double* ptr_y_grid_default, double* ptr_b_grid_lowr, double* ptr_b_grid_highr, double* ptr_p_grid, double* ptr_v, double* ptr_v_r, double* ptr_v_d, double* ptr_q_lowr, double* ptr_q_highr, int* ptr_b_policy_lowr, int* ptr_b_policy_highr, double* ptr_d_policy){
   
@@ -226,7 +226,7 @@ void Economy::update_vr_and_bond_policy(){
     }
 }
 
-/*
+
 // Solve the model:
 int Economy::solve_model(){
 
@@ -239,6 +239,7 @@ int Economy::solve_model(){
         mexPrintf("Economy initialization failed.\n");
         return EXIT_FAILURE;
     }
+
     // Guess value functions and prices and copy initial values:
     guess_vd_vr_q();    
     double* Vd0 = new double[Y_grid_size *  B_grid_size_highr * B_grid_size_lowr];     // Store initial value function at default:
@@ -316,8 +317,6 @@ int Economy::solve_model(){
     mexPrintf("Difference between low prices: %f\n", diff_q_lowr);
     mexPrintf("Difference between high prices: %f\n", diff_q_highr);
     mexPrintf("Threads: %d\n", omp_get_max_threads());
-    
-
     // Free memory:
     delete[] Vd0;
     delete[] Vr0;
@@ -326,4 +325,4 @@ int Economy::solve_model(){
     return EXIT_FAILURE;
 }
 
-*/
+
