aec656394eda37afb9b5b0d18e67352d3d9fb86a
BelmudesLSB <belmu002@umn.edu>
2024-01-23 17:33:21 -0600

    Storing results for OH. Switching costs are turned off for the final computations.

--- Mex_compilation.m
+++ Mex_compilation.m
@@ -14,13 +14,13 @@ mex CXXFLAGS="$CXXFLAGS -fopenmp -O3" LDFLAGS="$LDFLAGS -fopenmp" main.cpp econo
 %% Common parameters:
 
 %params.b_grid_size_lowr = 75;          % Number of points in the grid for the low recovery bond. 
-params.b_grid_size_highr = 81;         % Number of points in the grid for the high recovery bond. 
+params.b_grid_size_highr = 101;         % Number of points in the grid for the high recovery bond. 
 params.b_grid_min_lowr = 0.0;           % Minimum value for the low recovery bond grid.
 params.b_grid_min_highr = 0.0;          % Minimum value for the high recovery bond grid.
 params.b_grid_max_lowr = 0.75;           % Maximum value of the low recovery bond grid.
 params.b_grid_max_highr = 1;            % Maximum value of the high recovery bond grid.
 params.b_grid_size_lowr = (params.b_grid_max_lowr/params.b_grid_max_highr) * (params.b_grid_size_highr-1)+1;
-params.y_grid_size = 12;                 % Number of points in the grid for the income. (Always use Odd)
+params.y_grid_size = 11;                 % Number of points in the grid for the income. (Always use Odd)
 params.y_default = 0.969;               % Maximum income under default.
 params.beta = 0.953;                    % Discount factor.
 params.gamma = 2;                       % Risk aversion.
@@ -38,6 +38,4 @@ params.alpha_highr = 0.15;              % High recovery on defaulted debt.
 
 tic;
 calibrated_model_solution = main(params);
-save('Solution', 'calibrated_model_solution')
-save('Parameters', 'params')
 toc;
--- Total_bond_policy.m
+++ Total_bond_policy.m
@@ -2,4 +2,4 @@ calibrated_model_solution.B_policy_highr = calibrated_model_solution.B_policy_hi
 calibrated_model_solution.B_policy_lowr = calibrated_model_solution.B_policy_lowr + 1;
 Bond_policy = calibrated_model_solution.B_grid_highr(calibrated_model_solution.B_policy_highr) + calibrated_model_solution.B_grid_lowr(calibrated_model_solution.B_policy_lowr);
 Bond_policy(calibrated_model_solution.D_policy == 1) = NaN;
-Bond_policy = permute(reshape(Bond_policy, params.b_grid_size_lowr, params.b_grid_size_highr, params.y_grid_size), [2, 1, 3]);
+Bond_policy = permute(reshape(Bond_policy, params.b_grid_size_lowr, params.b_calibrated_model_solutiongrid_size_highr, params.y_grid_size), [2, 1, 3]);
